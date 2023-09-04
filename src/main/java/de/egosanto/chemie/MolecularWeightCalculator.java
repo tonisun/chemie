cb131b6948204c8f63e57f9f2ebdd373188ef7c9
Toni Zeidler <toni.z@gmx.de>
2023-09-04 14:18:14 +0200

    add doc

--- src/main/java/de/egosanto/chemie/MolecularWeightCalculator.java
+++ src/main/java/de/egosanto/chemie/MolecularWeightCalculator.java
@@ -16,6 +16,27 @@ import java.util.regex.Pattern;
  */
 public class MolecularWeightCalculator {
 
+    /**
+     * Berechnet das Molekulargewicht einer gegebenen chemischen Formel.
+     * <p>
+     * Die Funktion interpretiert eine chemische Formel (z.B. "H2O" für Wasser) und
+     * berechnet das Gesamtgewicht basierend auf den atomaren Gewichten der enthaltenen
+     * Elemente. Die chemischen Elemente sollten als Enums mit einer Methode atomGewicht()
+     * definiert sein, die das atomare Gewicht des Elements zurückgibt.
+     * </p>
+     *
+     * @param formula Die chemische Formel als Zeichenkette, z.B. "H2O", "C6H12O6", usw.
+     *                Die Formel sollte den Elementsymbolen folgen, optional gefolgt von
+     *                einer Zahl, die die Anzahl der Atome dieses Elements im Molekül angibt.
+     *                Wenn keine Zahl vorhanden ist, wird angenommen, dass das Element nur
+     *                einmal vorkommt.
+     *
+     * @return Das berechnete Molekulargewicht der gegebenen Formel.
+     *
+     * @throws IllegalArgumentException Wenn ein unbekanntes Elementsymbol in der Formel
+     *                                  gefunden wird oder wenn die Formel nicht dem erwarteten
+     *                                  Muster entspricht.
+     */
     public static double calculateMolecularWeight(String formula) {
         double totalWeight = 0;
 
